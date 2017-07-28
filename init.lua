216597d25ce9ea73a74d7d94a5db8bed3584b5c3
tenplus1 <tenplus1@users.noreply.github.com>
2017-07-28 09:00:44 +0100

    add moreores support
    added moreores tool support

--- init.lua
+++ init.lua
@@ -187,3 +187,36 @@ minetest.override_item("default:shovel_mese", {
   description = toolranks.create_description("Mese Shovel", 0, 1),
   after_use = toolranks.new_afteruse,
 })
+
+if minetest.get_modpath("moreores") then
+
+minetest.override_item("moreores:pick_mithril", {
+	original_description = "Mithril Pickaxe",
+	description = toolranks.create_description("Mithril Pickaxe", 0, 1),
+	after_use = toolranks.new_afteruse})
+
+minetest.override_item("moreores:axe_mithril", {
+	original_description = "Mithril Axe",
+	description = toolranks.create_description("Mithril Axe", 0, 1),
+	after_use = toolranks.new_afteruse})
+
+minetest.override_item("moreores:shovel_mithril", {
+	original_description = "Mithril Shovel",
+	description = toolranks.create_description("Mithril Shovel", 0, 1),
+	after_use = toolranks.new_afteruse})
+
+minetest.override_item("moreores:pick_silver", {
+	original_description = "Silver Pickaxe",
+	description = toolranks.create_description("Silver Pickaxe", 0, 1),
+	after_use = toolranks.new_afteruse})
+
+minetest.override_item("moreores:axe_silver", {
+	original_description = "Silver Axe",
+	description = toolranks.create_description("Silver Axe", 0, 1),
+	after_use = toolranks.new_afteruse})
+
+minetest.override_item("moreores:shovel_silver", {
+	original_description = "Silver Shovel",
+	description = toolranks.create_description("Silver Shovel", 0, 1),
+	after_use = toolranks.new_afteruse})
+end
